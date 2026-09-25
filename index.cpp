e5e6f1038cce27f1d12c5a0e271da9604380e315
Nagakarthik8 <nagakarthik248@gmail.com>
2026-09-25 22:54:14 +0530

    Update main function to print a welcome message
    Replaced multiple cout statements with a single welcome message.

--- index.cpp
+++ index.cpp
@@ -1,9 +1,6 @@
 #include<iostream>
 using namespace std;
 int main(){
-    cout << "Hello world";
-    cout << "Hello world 2";
-    cout << "Anil is topper";
-    cout << "mahesh is handsome";
+    cout << "Welcome to cpp" << endl;
     return 0;
 }
